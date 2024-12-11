da44e75ef3f4ef642d89ee83cd5da4e8dcd9d8af
matheusmpff <156636230+matheusmpff@users.noreply.github.com>
2024-12-10 23:12:00 -0300

    documentação da rb.c

--- rb.c
+++ rb.c
@@ -1,3 +1,14 @@
+/*Trabalho de Algoritmo e estrutura de dados.
+Docente: Rudinei Gularte
+Discentes: Matheus Muzza Pires Ferreira - 15479468, João Pedro Monteiro Castelli
+
+    O arquivo em questão fornece um dos TAD`S que serão utilizados no programa principal. A implementação proposta nesse arquivo 
+    é deu uma árvore binária que segue a estrutura de uma left-Leaning Red-Black Tree. Foram criadas certas funções auxiliares a
+    medida que foram surgindo necessidades para uma adequada implementação do TAD.
+
+    Referências: Material disponibilizado das aulas de AVL e das árvores LLRB.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -16,21 +27,23 @@ struct no{
     NO* raiz;
     int tamanho;
 };
-NO* propagaDir(NO* raiz);
-NO* removeMin(NO* raiz);
-NO* propagaEsq(NO* raiz);
-int auxPertence(NO* raiz, int elemento);
-NO* auxInserir(NO* raiz, int elemento);
-void inverteCor(NO* raiz);
-NO* rotDir(NO* raiz);
-NO* rotEsq(NO* raiz);
-void auxApagar(NO** raiz);
-int auxElementos(NO* raiz, int* vetor, int pos);
-NO* minDir(NO* raiz);
-NO* noCriar(int elemento);
-void auxUnir(NO* adicionar, NO* sintese);
-NO* auxRemover(NO* raiz, int elemento);
-void auxArrOrdenado(RB* sintese, int* elementos, int ini, int fim);
+
+
+NO* propagaDir(NO* raiz);//Faz a propagação da aresta vermelha para o lado direito
+NO* removeMin(NO* raiz);// Quando achamos o valor mínimo da árvore direita que já substituiu o número que queremos retirar, nós o removemos;
+NO* propagaEsq(NO* raiz);//Faz a propagação da aresta vermelha para o lado esquerdo para a remoção adequada de nós.
+int auxPertence(NO* raiz, int elemento);//verifica se o inteiro elemento pertence a árvore binária red-black
+NO* auxInserir(NO* raiz, int elemento);//Função que auxilia na inserção de um elemento na árvore binária red-black
+void inverteCor(NO* raiz);//inverte a cor de um nó e de seus respectivos filhos quando eles não forem nulos
+NO* rotDir(NO* raiz);// Rotaciona o nó para direita quando a árvore precisa ser balanceada
+NO* rotEsq(NO* raiz);// Rotaciona o nó para a esquerda quando a árvore precisa ser balanceada
+void auxApagar(NO** raiz);//Auxilia no momento de apagar a árvore binária red-black
+int auxElementos(NO* raiz, int* vetor, int pos);//Função que auxilia na criação de um vetor com os elementos da árvore binária red-black.
+NO* minDir(NO* raiz);//Encontra o menor valor que está a direita de um nó que queremos remover
+NO* noCriar(int elemento);//Cria um nó que será inserido na árvore binária
+void auxUnir(NO* adicionar, NO* sintese);//Função que une duas árvores binárias
+NO* auxRemover(NO* raiz, int elemento);//Função que auxilia na remoção de um dado elemento que está presente na árvore binária red-black
+void auxArrOrdenado(RB* sintese, int* elementos, int ini, int fim);//Função que transforma um array ordenado na árvore binária red-black
 
 NO* noCriar(int elemento){
     NO* n = malloc(sizeof(NO));
@@ -81,7 +94,7 @@ NO* rotEsq(NO* raiz){
 
 
 
-
+/*Cria a struct que auxiliará na manipulação da árvore.*/
 RB* rbCriar(void){
     RB * rb = malloc(sizeof(RB));
     if(rb != NULL){
@@ -90,7 +103,11 @@ RB* rbCriar(void){
     }
     return rb;
 }
-
+/*Função responsável por ver se o elemento pertence a uma árvore fornecida.
+    Saída: 
+        true-> se pertence a árvore
+        False->Se não pertence a árvore
+ */
 bool rbPertence(RB* rb, int elemento ){
     if(auxPertence(rb->raiz,elemento)){
         return true;
@@ -99,7 +116,7 @@ bool rbPertence(RB* rb, int elemento ){
         return false;
     }
 }
-
+/*Retorna 0 se o elemento não está contido na árvore, ou retorna 1 caso contrário */
 int auxPertence(NO* raiz, int elemento){
     if(raiz == NULL){
         return 0;
@@ -115,7 +132,7 @@ int auxPertence(NO* raiz, int elemento){
     
     return auxPertence(raiz->dir,elemento);
 }
-
+/*Insere o elemento na árvore binária */
 bool rbInserir(RB* rb, int elemento){
     if(rbPertence(rb,elemento)){
         return false;
@@ -129,7 +146,7 @@ bool rbInserir(RB* rb, int elemento){
         return false;
     }
 }
-
+/*Analisa se um dado nó da árvore está ou não recebendo uma aresta vermelha */
 int Vermelho(NO* raiz){
     if(raiz == NULL){
         return 0;
@@ -137,6 +154,8 @@ int Vermelho(NO* raiz){
 
     return (raiz->cor == 1);
 }
+/*Auxilia na inserção criando o nó e o colocando na posição correta, além de fazer os ajustes necessários, caso a árvore fique 
+desbalanceada após a inserção */
 NO* auxInserir(NO* raiz, int elemento){
    if(raiz == NULL){
         return noCriar(elemento);
@@ -164,13 +183,17 @@ NO* auxInserir(NO* raiz, int elemento){
 
 
 }
-
+/*Remove um elemento desejado da árvore que for passada por argumento. Faz os deslocamentos necessários das arestas vermelhas 
+para esse processo. Além de fazer as correções necessárias quando ficarem desbalanceadas. Retorna true se o elemento foi retirado, 
+e retorna false se o elemento pedido não estiver presente na árvore ou se não foi possível retirá-lo*/
 bool rbRemover(RB* rb, int elemento){
     if(!rbPertence(rb, elemento)){
         return false;
     }
     if((rb->raiz = auxRemover(rb->raiz, elemento) )!= NULL){
+        rb->tamanho--;
         return true;
+        
     }
     return false;
 }
@@ -182,6 +205,8 @@ NO* auxRemover(NO* raiz, int elemento){
     }
 
     if(raiz->dado == elemento){
+        /*Se o nó a ser retirado tem algum dos filhos como nulo, basta deslocar o filho não nulo para o local do nó pai,
+         e apagar o nó pai, caso os dois sejam nulos um deles substituirá o pai e podemos apagá-lo em seguida  */
         if(raiz->esq == NULL || raiz->dir == NULL){
             NO* aux = raiz;
             if(raiz->esq == NULL){
@@ -193,11 +218,14 @@ NO* auxRemover(NO* raiz, int elemento){
             free(aux);
             aux = NULL;
         }
+        /*Se o nó a ser retirado tem os dois filhos presentes, encontraremos o menor elemento da árvore direita e substituir 
+        com o nó a ser retirado,precisamos propagar a aresta vermelha até o local de remoção,ou seja onde estava o menor elemento 
+        da árvore direita, depois, removemos o menor elemento que estava duplicado por ter substituido o elemento a ser retirado*/  
         else{
             raiz = propagaDir(raiz);
-            NO* aux = minDir(raiz->dir);
+            NO* aux = minDir(raiz->dir);//encontra o minimo elemento da direita
             raiz->dado = aux->dado;
-            raiz->dir = removeMin(raiz->dir);
+            raiz->dir = removeMin(raiz->dir);// percorre até o local do minimo e o retira
         }
     }
     else{
@@ -211,7 +239,7 @@ NO* auxRemover(NO* raiz, int elemento){
             raiz->dir = auxRemover(raiz->dir,elemento);
         }
     }
-
+    /*Parte de ajuste no retorno da recursão para corrigir algum desalinho com a estrutura da árvore LLRB*/
     if(raiz!= NULL){
         if(Vermelho(raiz->dir) ==1 && !Vermelho(raiz->esq) ){
         raiz = rotEsq(raiz);
@@ -278,7 +306,7 @@ NO* minDir(NO* raiz){
     }
     return minDir(raiz->esq);
 }
-
+//apafa a árvore toda
 void rbApagar(RB **rb){
     auxApagar(&(*rb)->raiz);
     free(*rb);
@@ -286,7 +314,7 @@ void rbApagar(RB **rb){
 
     return;
 }
-
+//Auxilia no processo recursivo para apagar os elementos
 void auxApagar(NO**raiz){
     if(*raiz == NULL){
         return;
@@ -300,20 +328,18 @@ void auxApagar(NO**raiz){
 
     return;
 }
-
+/*Função responsável por criar e retornar um array de inteiros que estão presentes na árvore que será passada como argumento.*/
 int *rbElementos(RB* rb){
    int* vetor = malloc(sizeof(int)*rb->tamanho);
 
-    for(int i=0;i<rb->tamanho;i++){
-        vetor[i] = -1;
-    }
     if( vetor == NULL){
         return vetor;
     }
     auxElementos(rb->raiz,vetor,0);
     return vetor;
 }
-
+/*Auxilia na parte recursiva para inserir os elementos de forma ordenada no vetor que será retornado, para isso usamos um caminho 
+que a interação com o nó é feita entre as chamadas recursivas */
 int auxElementos(NO* raiz, int* vetor, int pos){
     if(raiz == NULL){
         return pos;
@@ -327,16 +353,16 @@ int auxElementos(NO* raiz, int* vetor, int pos){
     pos = auxElementos(raiz->dir, vetor, pos);
     return pos;
 }
-
+//Retorna a quantidade de elementos da árvore
 int rbTamanho(RB *rb){
     return rb->tamanho;
 }
-
+//retorna a árvore síntese que irá agora ter seus elementos e os elementos da árvore rb, que serão passados para a função.
 RB* rbUnir(RB *sintese, RB *rb){
     auxUnir(rb->raiz,sintese->raiz);
     return sintese;
 }
-
+//Função que cria uma árvore LLRB tomando como base os elementos de um vetor que será passado para a função
 RB* rbArrOrdenado( int* elementos, int tamanho){
     RB* sintese = rbCriar();
 
@@ -365,7 +391,7 @@ void auxArrOrdenado(RB* sintese, int* elementos, int ini, int fim){
     }
     
 }
-
+//Une duas árvores em uma só, que será o retorno da função principal. A árvore síntese será a árvore que conterá os elementos de ambas.
 void auxUnir(NO* adicionar, NO* sintese){
     if(adicionar == NULL){
         return;
@@ -377,7 +403,7 @@ void auxUnir(NO* adicionar, NO* sintese){
     auxInserir(sintese, adicionar->dado);
     return;
 }
-
+//função para percorrer a árvore e mostrar na tela todos os elementos
 void printar(NO* rb){
 
     if(rb == NULL){
@@ -386,49 +412,4 @@ void printar(NO* rb){
     printf("%d ",rb->dado);
     printar(rb->esq);
     printar(rb->dir);
-}
-
-int main(){
-    RB* rb = rbCriar();
-    RB * rb2 = rbCriar();
-
-    if(rb== NULL || rb2 == NULL){
-        printf("MERDA");
-        exit(1);
-    }
-    rbInserir(rb, 1);
-    rbInserir(rb,2);
-    rbInserir(rb,3);
-    rbInserir(rb,4);
-    rbInserir(rb,12);
-    rbInserir(rb, 32);
-    rbInserir(rb,14);
-
-    rbInserir(rb2, 5);
-    rbInserir(rb2,6);
-    rbInserir(rb2,13);
-    rbInserir(rb2,43);
-    
-    printar(rb2->raiz);
-    printf("\n");    
-    printar(rb->raiz);
-    printf("\n");
-    RB* uniao = rbUnir(rb,rb2);
-    
-   printar(uniao->raiz);
-   printf("\n");
-
-   rbRemover(uniao,12);
-   printar(uniao->raiz);
-    printf("\n");
-    int * vet = rbElementos(uniao);
-
-    for(int i = 0;i<uniao->tamanho;i++){
-        printf("%d ", vet[i]);
-    }
-
-   rbApagar(&rb);
-   rbApagar(&rb2);
-    
-    return 0;
 }
